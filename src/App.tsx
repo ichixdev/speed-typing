438c8105b2b9e6bc6a7e19e0efcbf2df95426065
Luis Moncada <luiismoncada@gmail.com>
2024-02-29 18:53:58 -0400

    added restartbutton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-function App() {
-  return <div className="text-4xl text-center text-blue-500">It works</div>;
-}
+import { faker } from '@faker-js/faker';
+import RestartButton from './components/RestartButtom';
+
+const words = faker.word.words(10);
+
+const App = () => {
+  return (
+    <>
+      <CountdownTimer timeLeft={30}></CountdownTimer>
+      <GenerateWords words={words}></GenerateWords>
+      <RestartButton
+        className='"mx-auto mt-10 text-slate-500'
+        onRestart={() => null}
+      ></RestartButton>
+    </>
+  );
+};
+
+const GenerateWords = ({ words }: { words: string }) => {
+  return <div className="text-4xl text-center text-slate-500">{words}</div>;
+};
+
+const CountdownTimer = ({ timeLeft }: { timeLeft: number }) => {
+  return <h2 className="text-primary-400 font-medium">Time:{timeLeft}</h2>;
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.yellow,
+      },
+    },
   },
   plugins: [],
 };
